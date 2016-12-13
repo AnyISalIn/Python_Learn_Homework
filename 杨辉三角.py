f9ca5131a0f1b56b289506c2cebeba203b8bdff7
AnyISalIn <anyisalin@gmail.com>
2016-12-13 15:59:05 +0800

    完成杨辉三角

---
+++ 杨辉三角.py
@@ -0,0 +1,23 @@
+def yhsj(layer):
+    lst = []
+    for i in range(0, layer):
+        if i == 0:
+            lst.append([1]); yield lst[0]
+        else:
+            lst.append([1])
+            for number in range(1, i):
+                lst[i].append(lst[i-1][number-1] + lst[i-1][number])
+            lst[i].append(1)
+            yield lst[i]
+
+def main(layer):
+    max_len = len(' '.join(str(x) for x in list(yhsj(layer))[-1]))
+    c = 0
+    for i in yhsj(layer):
+        string = ' '.join([ str(x) for x in i ])
+        c += 1
+        yield(string.center(max_len))
+
+if __name__ == '__main__':
+    for i in main(10):
+        print(i)
